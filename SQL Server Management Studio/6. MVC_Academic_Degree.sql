13e24f105b6f2084839b1a59bab14e4aad753ed0
coldaniel88 <daniel.prendas@ucr.ac.cr>
2021-05-10 16:08:44 -0600

    Query Academic Degree
    Query 6 Academic Degree

---
+++ SQL Server Management Studio/6. MVC_Academic_Degree.sql
@@ -0,0 +1,11 @@
+INSERT INTO AcademicDegree( Name)
+VALUES ('Bachillerato')
+
+INSERT INTO AcademicDegree(Name)
+VALUES ('Licenciatura')
+
+INSERT INTO AcademicDegree(Name)
+VALUES ('Maestría')
+
+INSERT INTO AcademicDegree(Name)
+VALUES ('Doctorado')
