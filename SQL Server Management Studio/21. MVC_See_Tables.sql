036c44308f049389af9b8c12cb8f3e8b9d2ce481
David Robles <jdrobles0110@gmail.com>
2021-05-19 23:30:05 -0600

    sql modification

--- SQL Server Management Studio/21. MVC_See_Tables.sql
+++ SQL Server Management Studio/21. MVC_See_Tables.sql
@@ -1,8 +1,9 @@
 use [MVC_IF4101MVCLPAC]
 select * from AcademicDegree
 select * from Appointment
-select * from Canton
 select * from Course
+select * from Province
+select * from Canton
 select * from District
 select * from Location
 select * from PrivateMessage
@@ -17,5 +18,3 @@ select * from SocialNetworksStudent
 select * from Student
 select * from StudentCourse
 select * from Users
-
-Scaffold-DbContext "Server=163.178.107.10;Initial Catalog=MVC_IF4101MVCLPAC;User ID=laboratorios;Password=KmZpo.2796" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models/Entities -Tables AcademicDegree,Appointment,Canton,Course,District,Location,PrivateMessage,Professor,ProfessorCourse,Province,PublicConsultation,RepliesPrivateMessage,RepliesPublicConsultation,SocialNetworksCatalog,SocialNetworksStudent,Student,StudentCourse,Users
