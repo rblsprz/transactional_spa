5e8c55f31008fb9933d81447d08a0bf5e2533b81
coldaniel88 <daniel.prendas@ucr.ac.cr>
2021-05-26 03:47:57 -0600

    Add AutomatedTestSelenium
    Automated Test with Selenium

---
+++ AutomatedTestsSelenium/SeleniumTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Opera;
+
+
+
+namespace SeleniumTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new OperaDriver();
+            driver.Navigate().GoToUrl("https://localhost:44346");
+            driver.Manage().Window.Maximize();
+
+        }
+    }
+}
